c89de8e9eff0e5e5dfa854ca42a52340ec2c6b93
Kai Waehner <megachucky@gmail.com>
2018-06-25 14:27:45 +0200

    KSQL Script not working well with 'doitlive' tool => Added notes

--- ksql-live-demo.sh
+++ ksql-live-demo.sh
@@ -54,9 +54,11 @@ SELECT * FROM users_original LIMIT 10;
 CREATE TABLE FEMALEUSERS AS SELECT * from users_original WHERE gender = 'FEMALE';
 SELECT * FROM users_original WHERE gender = 'MALE' LIMIT 3;
 
-CREATE STREAM pageviews_female AS SELECT users_original.userid AS userid, pageid, regionid, gender FROM pageviews_original LEFT JOIN users_original ON pageviews_original.userid = users_original.userid WHERE gender = 'FEMALE';
+#CREATE STREAM pageviews_female AS SELECT users_original.userid AS userid, pageid, regionid, gender FROM pageviews_original LEFT JOIN users_original ON pageviews_original.userid = users_original.userid WHERE gender = 'FEMALE';
 
-SELECT * FROM pageviews_female LIMIT 3;
+#SELECT * FROM pageviews_female LIMIT 3;
+
+LIST TOPICS;
 
 PRINT 'ratings' FROM BEGINNING;
 
